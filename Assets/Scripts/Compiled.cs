2002badadb5d47c5666af7b2d4aa11749187e768
notrllyxyion <mikelreuelramos69@gmail.com>
2025-05-07 22:44:28 +0800

    Initial commit

---
+++ Assets/Scripts/Compiled.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Compiled : MonoBehaviour
+{
+    public InputField AddInventory;
+    public InputField RemoveInventory;
+    public Text Inventory;
+
+    static List<string> inventory = new List<string>();
+
+    public void ViewInventory()
+    {
+      
+    }
+}
