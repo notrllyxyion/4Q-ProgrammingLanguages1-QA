7dee0e96156dab68733494582f4eb67960bed030
notrllyxyion <mikelreuelramos69@gmail.com>
2025-05-09 10:22:09 +0800

    all

--- Assets/Scripts/Compiled.cs
+++ Assets/Scripts/Compiled.cs
@@ -11,8 +11,42 @@ public class Compiled : MonoBehaviour
 
     static List<string> inventory = new List<string>();
 
+    public void Additem()
+    {
+      string item = AddInventory.text;
+        if (!string .IsNullOrEmpty(item) )
+        {
+            inventory.Add(item);
+            AddInventory.text = "";
+            ViewInventory();
+
+        }
+    }
+    public void RemoveItem()
+    {
+        string item = RemoveInventory.text;
+        if (!string.IsNullOrEmpty(item) && inventory.Contains(item))
+        {
+            inventory.Remove(item);
+            RemoveInventory.text = "";
+            ViewInventory();
+
+        }
+    }
     public void ViewInventory()
     {
-      
+        if (inventory.Count > 0)
+        {
+            Inventory.text = "";
+            for (int i = 0; i < inventory.Count; i++)
+            {
+                Inventory.text += $"{i + 1}. {inventory[i]}\n";
+            }
+        }
+        else
+        {
+            Inventory.text = "Inventory is empty.";
+        }
     }
+
 }
